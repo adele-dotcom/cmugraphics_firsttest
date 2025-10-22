3da8144871a24b99689f554bc08b5770cd60c235
adele-dotcom <adele10ads@gmail.com>
2025-10-22 11:14:53 -0700

    Cmu testing

---
+++ main.py
@@ -0,0 +1,5 @@
+from cmu_graphics import *
+### Your code goes here; if it contains a call to
+### runApp(), use the "CPCS Mode" starter code instead.
+Circle(100,100,200,fill='red')
+cmu_graphics.run()
